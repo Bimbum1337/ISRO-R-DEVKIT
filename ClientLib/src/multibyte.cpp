f748970e009d7d541ec94d95670c1c192cde534e
florian0 <florian0@protonmail.com>
2019-01-29 18:20:36 +0100

    [1/1] Update multibyte string conversions for supporting n_string and n_wstring

--- ClientLib/src/multibyte.cpp
+++ ClientLib/src/multibyte.cpp
@@ -15,29 +15,110 @@ std::string acp_encode(const wchar_t* text, size_t length)
     return strTo;
 }
 
-std::string acp_encode(const std::wstring &wstr)
+std::n_string acp_n_encode(const wchar_t* text, size_t length)
 {
-	return acp_encode(wstr.c_str(), wstr.length());
+	if( length == 0 ) return std::n_string();
+    int size_needed = WideCharToMultiByte(CP_ACP, 0, text, length, NULL, 0, NULL, NULL);
+    std::n_string strTo( size_needed, 0 );
+    WideCharToMultiByte(CP_ACP, 0, text, length, &strTo[0], size_needed, NULL, NULL);
+    return strTo;
 }
 
-std::string acp_encode(const std::wstring *wstr)
+std::string acp_encode(const std::wstring& wstr)
+{
+	return acp_encode(&wstr);
+}
+
+std::string acp_encode(const std::wstring* wstr)
 {
 	return acp_encode(wstr->c_str(), wstr->length());
 }
 
-std::wstring acp_decode(const std::string& str)
+std::string acp_encode(const std::n_wstring* wstr)
 {
-    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+	return acp_encode(wstr->c_str(), wstr->length());
+}
+
+std::string acp_encode(const std::n_wstring& wstr)
+{
+	return acp_encode(&wstr);
+}
+
+std::n_string acp_n_encode(const std::wstring* wstr)
+{
+	return acp_n_encode(wstr->c_str(), wstr->length());
+}
+
+std::n_string acp_n_encode(const std::wstring& wstr)
+{
+	return acp_n_encode(&wstr);
+}
+
+std::n_string acp_n_encode(const std::n_wstring* wstr)
+{
+	return acp_n_encode(wstr->c_str(), wstr->length());
+}
+
+std::n_string acp_n_encode(const std::n_wstring& wstr)
+{
+	return acp_n_encode(&wstr);
+}
+
+std::wstring acp_decode(const char* str, size_t length)
+{
+	if (length == 0) return std::wstring();
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], length, NULL, 0);
     std::wstring wstrTo( size_needed, 0 );
-    MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], length, &wstrTo[0], size_needed);
     return wstrTo;
 }
 
-std::wstring acp_decode(char* str)
+std::n_wstring acp_n_decode(const char* str, size_t length)
 {
-	int len = strlen(str);
-    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], len, NULL, 0);
-    std::wstring wstrTo( size_needed, 0 );
-    MultiByteToWideChar(CP_UTF8, 0, &str[0], len, &wstrTo[0], size_needed);
+	if (length == 0) return std::n_wstring();
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], length, NULL, 0);
+    std::n_wstring wstrTo( size_needed, 0 );
+    MultiByteToWideChar(CP_UTF8, 0, &str[0], length, &wstrTo[0], size_needed);
     return wstrTo;
 }
+
+std::wstring acp_decode(const std::string& str)
+{
+	return acp_decode(&str);
+}
+
+std::wstring acp_decode(const std::string* str)
+{
+	return acp_decode(str->c_str(), str->length());
+}
+
+std::n_wstring acp_n_decode(const std::string& str)
+{
+	return acp_n_decode(&str);
+}
+
+std::n_wstring acp_n_decode(const std::string* str)
+{
+	return acp_n_decode(str->c_str(), str->length());
+}
+
+std::wstring acp_decode(const std::n_string& str)
+{
+	return acp_decode(&str);
+}
+
+std::wstring acp_decode(const std::n_string* str)
+{
+	return acp_decode(str->c_str(), str->length());
+}
+
+std::n_wstring acp_n_decode(const std::n_string& str)
+{
+	return acp_n_decode(&str);
+}
+
+std::n_wstring acp_n_decode(const std::n_string* str)
+{
+	return acp_n_decode(str->c_str(), str->length());
+}
+
