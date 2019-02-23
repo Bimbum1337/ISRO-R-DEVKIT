a9e811b58172a6cfffa931f78ed689c714c6b55a
florian0 <florian0@protonmail.com>
2019-02-23 19:57:50 +0100

    Move project root out of source folder

--- helpers/make_project_vs2010.cmd
+++ helpers/make_project_vs2010.cmd
@@ -2,4 +2,4 @@
 
 call "%VS80COMNTOOLS%vsvars32.bat"
 
-cmake -G "Visual Studio 10 2010" -T v80 -DCMAKE_BUILD_TYPE=RelWithDebInfo ../source/
+cmake -G "Visual Studio 10 2010" -T v80 -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
