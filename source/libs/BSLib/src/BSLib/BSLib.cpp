457934280169fdfc361ca0866b812bf69bb2e3b6
florian0 <florian0@protonmail.com>
2020-03-17 23:49:06 +0100

    Reformat BSLib

--- source/libs/BSLib/src/BSLib/BSLib.cpp
+++ source/libs/BSLib/src/BSLib/BSLib.cpp
@@ -15,88 +15,76 @@
 
 const GlobalVar<CGfxRuntimeClass, 0x0110F8FC> CObj::classCObj;
 
-CGfxRuntimeClass const* CObj::GetRuntimeClass() const
-{
-	return classCObj;
+CGfxRuntimeClass const *CObj::GetRuntimeClass() const {
+    return classCObj;
 }
 
-CGfxRuntimeClass const* CObj::GetParentRuntimeClass() const
-{
-	return classCObj->m_pBaseClass;
+CGfxRuntimeClass const *CObj::GetParentRuntimeClass() const {
+    return classCObj->m_pBaseClass;
 }
 
-CObj* PASCAL CObj::CreateObject()
-{
-	return new CObj; 
+CObj *PASCAL
+
+CObj::CreateObject() {
+    return new CObj;
 }
 
-void __cdecl CObj::DeleteObject(CObj* pObj)
-{
-	delete pObj; 
+void __cdecl CObj::DeleteObject(CObj *pObj) {
+    delete pObj;
 }
 
-CObj::~CObj()
-{
+CObj::~CObj() {
 
 }
 
 const GFX_MSGMAP CObjChild::messageMapCObjChild = {
-	0, 
-	GetThisMessageEntries()
+        0,
+        GetThisMessageEntries()
 };
 
 // CGfxRuntimeClass::IsDerivedFrom(CRuntimeClass *) .text 00B94B60 00000008 00000000 00000000 R . . . . . .
-BOOL CGfxRuntimeClass::IsDerivedFrom(const CGfxRuntimeClass* pBaseClass) const
-{
-	assert(FALSE);
-	return FALSE;
+BOOL CGfxRuntimeClass::IsDerivedFrom(const CGfxRuntimeClass *pBaseClass) const {
+    assert(FALSE);
+    return FALSE;
 }
 
-const GFX_MSGMAP* CObjChild::GetMessageMap() const
-{
-	return &messageMapCObjChild;
+const GFX_MSGMAP *CObjChild::GetMessageMap() const {
+    return &messageMapCObjChild;
 }
 
-const GFX_MSGMAP_ENTRY* CObjChild::GetThisMessageEntries()
-{
-	static const GFX_MSGMAP_ENTRY _messageEntries[] = 
-	{
-		{0, 0, 0, 0, 0, 0, (GFX_PMSG)0, 0, 0, 0 }
-	};
-	return &_messageEntries[0];
+const GFX_MSGMAP_ENTRY *CObjChild::GetThisMessageEntries() {
+    static const GFX_MSGMAP_ENTRY _messageEntries[] =
+            {
+                    {0, 0, 0, 0, 0, 0, (GFX_PMSG) 0, 0, 0, 0}
+            };
+    return &_messageEntries[0];
 }
 
 // CObjChild::Func_4(void) .text 0046FDA0 00000006 00000000 00000000 R . . . . . .
-int CObjChild::Func_4(int a2)
-{
-	return -1;
+int CObjChild::Func_4(int a2) {
+    return -1;
 }
 
 // CObjChild::OnTimer(void) .text 0046FD90 00000003 00000000 00000000 R . . . . . .
-void CObjChild::OnTimer(int)
-{
-	// empty
+void CObjChild::OnTimer(int) {
+    // empty
 }
 
 // CObjChild::OnNetMsg(void) .text 0046FD80 00000005 00000000 00000000 R . . . . . .
-BOOL CObjChild::OnNetMsg(CMsgStreamBuffer*)
-{
-	return FALSE;
+BOOL CObjChild::OnNetMsg(CMsgStreamBuffer *) {
+    return FALSE;
 }
 
-CObjChild::CObjChild()
-{
-	// empty
+CObjChild::CObjChild() {
+    // empty
 }
 
-/*
-void* CObj::operator new(size_t num_bytes)
-{
+# if 0
+void* CObj::operator new(size_t num_bytes) {
 	return CMemPoolManager_Allocate(num_bytes);
 }
 
-void CObj::operator delete(void* data)
-{
+void CObj::operator delete(void* data) {
 	CMemPoolManager_Free(data);
 }
-*/
+#endif
--- source/libs/BSLib/src/BSLib/BSLib.h
+++ source/libs/BSLib/src/BSLib/BSLib.h
@@ -1,14 +1,18 @@
 #pragma once
+
 #include <Windows.h>
 
 #include <remodel/GlobalVar.h>
 #include <ClientNet/MsgStreamBuffer.h>
 
 class CWhatever;
+
 struct CGfxRuntimeClass;
 struct GFX_MSGMAP;
+
 class CObj;
-	class CObjChild;
+
+class CObjChild;
 
 
 #include "_internal/custom_stl.h"
@@ -18,184 +22,183 @@ class CObj;
 
 
 /// Common base class for runtime-identifiable objects
-class CObj
-{
+class CObj {
 public:
-	static const GlobalVar<CGfxRuntimeClass, 0x0110F8FC> classCObj;
+    static const GlobalVar<CGfxRuntimeClass, 0x0110F8FC> classCObj;
 
-	virtual CGfxRuntimeClass const* GetRuntimeClass() const;
-	virtual CGfxRuntimeClass const* GetParentRuntimeClass() const;
+    virtual CGfxRuntimeClass const *GetRuntimeClass() const;
 
-	/// Create a new object of this type. This needs to be created for every derived class.
-	/// \returns Pointer to the created object
-	static CObj* __stdcall CreateObject();
-    
-	/// Delete an existing object of this type. This needs to be created for every derived class.
-	/// Usually just calls the (virtual) destructor.
-	/// \param pObj Object to delete
-	static void __cdecl DeleteObject(CObj* pObj);
-	
+    virtual CGfxRuntimeClass const *GetParentRuntimeClass() const;
+
+    /// Create a new object of this type. This needs to be created for every derived class.
+    /// \returns Pointer to the created object
+    static CObj *__stdcall CreateObject();
+
+    /// Delete an existing object of this type. This needs to be created for every derived class.
+    /// Usually just calls the (virtual) destructor.
+    /// \param pObj Object to delete
+    static void __cdecl DeleteObject(CObj *pObj);
+
 public:
-	virtual ~CObj();
+    virtual ~CObj();
 
-	// void* operator new(size_t num_bytes);
+#if 0
+    void* operator new(size_t num_bytes);
+#endif
 };
 
 /// Meta-data-structure for runtime-identifiable objects. Every CObj-derived 
 /// class has a global instance of this structure containing all relevant meta 
 /// data.
 /// Funfact: Joymax stole this class from afx.h and renamed various macros. LOL.
-struct CGfxRuntimeClass
-{
-	/// Name of the class
-	const char * m_lpszClassName;
+struct CGfxRuntimeClass {
+    /// Name of the class
+    const char *m_lpszClassName;
 
-	/// Size of the class in bytes
-	int m_nObjectSize;
-	
-	/// Absolutely no clue
-	unsigned int m_wSchema; // schema number of the loaded class
+    /// Size of the class in bytes
+    int m_nObjectSize;
 
-	/// Pointer to the meta-base class
-	CGfxRuntimeClass const *m_pBaseClass;
+    /// Absolutely no clue
+    unsigned int m_wSchema; // schema number of the loaded class
 
-	int field_10;
-	int field_14;
+    /// Pointer to the meta-base class
+    CGfxRuntimeClass const *m_pBaseClass;
 
-	/// Function pointer to the \ref CObj::CreateObject equivalent function
-	CObj*(__stdcall *m_pfnCreateObject)();
-    
-	/// Function pointer to the \ref CObj::DeleteObject equivalent function
-	void(__cdecl *m_pfnDeleteObject)(CObj*);
+    int field_10;
+    int field_14;
 
-	/// Evaluate if the current class is derived from the given class
-	BOOL IsDerivedFrom(const CGfxRuntimeClass* pBaseClass) const;
+    /// Function pointer to the \ref CObj::CreateObject equivalent function
+    CObj *(__stdcall *m_pfnCreateObject)();
 
-	/// Initialize a new instance of this class. 
+    /// Function pointer to the \ref CObj::DeleteObject equivalent function
+    void (__cdecl *m_pfnDeleteObject)(CObj *);
+
+    /// Evaluate if the current class is derived from the given class
+    BOOL IsDerivedFrom(const CGfxRuntimeClass *pBaseClass) const;
+
+    /// Initialize a new instance of this class.
     /// This was probably the constructor.
-	void BuildThunk(const char* str, CObj*(__stdcall * create_object)(), void(__cdecl * delete_object)(CObj*), CGfxRuntimeClass* baseclass, size_t size, int x) const;
+    void BuildThunk(const char *str, CObj *(__stdcall *create_object)(), void(__cdecl *delete_object)(CObj *),
+                    CGfxRuntimeClass *baseclass, size_t size, int x) const;
 
-	// dynamic name lookup and creation
-	// static CGfxRuntimeClass* __stdcall FromName(const char * lpszClassName);
-	// static CGfxRuntimeClass* __stdcall FromName(const wchar_t * lpszClassName);
-	// static CObj* __stdcall CreateObject(const char * lpszClassName);
-	// static CObj* __stdcall CreateObject(const wchar_t * lpszClassName);
+    // dynamic name lookup and creation
+    // static CGfxRuntimeClass* __stdcall FromName(const char * lpszClassName);
+    // static CGfxRuntimeClass* __stdcall FromName(const wchar_t * lpszClassName);
+    // static CObj* __stdcall CreateObject(const char * lpszClassName);
+    // static CObj* __stdcall CreateObject(const wchar_t * lpszClassName);
 };
 
-class CWhatever
-{
-	int x[5];
+class CWhatever {
+    int x[5];
 };
 
 /// Some kind of intermediate base class. Contains message handling, networking
 /// and timers ...
-class CObjChild : public CObj
-{
+class CObjChild : public CObj {
 public:
-	const virtual GFX_MSGMAP* GetMessageMap() const;
-	static const struct GFX_MSGMAP_ENTRY* GetThisMessageEntries(); \
-	static const GFX_MSGMAP messageMapCObjChild;
+    const virtual GFX_MSGMAP *GetMessageMap() const;
 
-	virtual int Func_4(int a2);
+    static const struct GFX_MSGMAP_ENTRY *GetThisMessageEntries();
 
-	/// Callback when a timer elapses
-	/// \param timerId Numeric identifier of the timer
-	virtual void OnTimer(int timerId);
+    static const GFX_MSGMAP messageMapCObjChild;
 
-	/// Callback when a network msg arrives
-	/// \param pMsg Pointer to the received message. This is not essentially a
+    virtual int Func_4(int a2);
+
+    /// Callback when a timer elapses
+    /// \param timerId Numeric identifier of the timer
+    virtual void OnTimer(int timerId);
+
+    /// Callback when a network msg arrives
+    /// \param pMsg Pointer to the received message. This is not essentially a
     ///             a server network message. Some MsgIds appear weird.
     ///             See \ref CPSQuickStart::OnNetMsg for example MsgIds.
     /// \returns true, if the message was handled, false, otherwise. A message 
     ///          must be completely read before returning. Call 
     ///          \ref CMsgStreamBuffer::FlushRemaining if there are any bytes 
     ///          left over.
-	virtual BOOL OnNetMsg(CMsgStreamBuffer* pMsg);
+    virtual BOOL OnNetMsg(CMsgStreamBuffer *pMsg);
 
-	CObjChild();
+    CObjChild();
 
 private:
-	char pad_0004[16];
-	std::n_list<CWhatever> m_list;
+    char pad_0004[16];
+    std::n_list<CWhatever> m_list;
 };
 
 
 /// Dummy class that serves as castable target class for message handlers
-class CCmdTarget : public CObj
-{
+class CCmdTarget : public CObj {
 };
 
 typedef void (CCmdTarget::*GFX_PMSG)(void);
 
 /// Entry of a message map array. Each entry specifies one function.
-struct GFX_MSGMAP_ENTRY
-{
-	/// windows message
-	UINT nMessage;
-	/// control code or WM_NOTIFY code
-	UINT nCode; 
-	/// control ID (or 0 for windows messages)
-	UINT nID; 
-	/// used for entries specifying a range of control id's
-	UINT nLastID; 
-	/// signature type (action) or pointer to message #
-	UINT_PTR nSig;
-	UINT nU0;
-	/// routine to call (or special value)
-	GFX_PMSG pfn; 
-	UINT nU1;
-	UINT nU2;
-	UINT nU3;
+struct GFX_MSGMAP_ENTRY {
+    /// windows message
+    UINT nMessage;
+    /// control code or WM_NOTIFY code
+    UINT nCode;
+    /// control ID (or 0 for windows messages)
+    UINT nID;
+    /// used for entries specifying a range of control id's
+    UINT nLastID;
+    /// signature type (action) or pointer to message #
+    UINT_PTR nSig;
+    UINT nU0;
+    /// routine to call (or special value)
+    GFX_PMSG pfn;
+    UINT nU1;
+    UINT nU2;
+    UINT nU3;
 };
 
 
 /// Message map of a class. Likely to be present when deriving from 
 /// \ref CObjChild . Only present when the class implements custom message 
 /// handlers. 
-struct GFX_MSGMAP
-{
-	/// Pointer to the base message map entry.
-	const GFX_MSGMAP* lpBaseMsgMap;
+struct GFX_MSGMAP {
+    /// Pointer to the base message map entry.
+    const GFX_MSGMAP *lpBaseMsgMap;
 
-	/// Pointer to the array of message map entires.
-	const GFX_MSGMAP_ENTRY* lpEntries;
+    /// Pointer to the array of message map entires.
+    const GFX_MSGMAP_ENTRY *lpEntries;
 };
 
 
 #define GFX_DECLARE_MESSAGE_MAP(theClass) \
 protected: \
-	static const GFX_MSGMAP_ENTRY* GetThisMessageEntries(); \
-	const GFX_MSGMAP* GetMessageMap() const override; \
-	static const GFX_MSGMAP messageMap##theClass;
+    static const GFX_MSGMAP_ENTRY* GetThisMessageEntries(); \
+    const GFX_MSGMAP* GetMessageMap() const override; \
+    static const GFX_MSGMAP messageMap##theClass;
 
 #define GFX_BEGIN_MESSAGE_MAP(theClass, baseClass) \
-	const GFX_MSGMAP theClass::messageMap##theClass = { &baseClass::messageMap##baseClass, GetThisMessageEntries()};\
-	const GFX_MSGMAP* theClass :: GetMessageMap() const \
-		{ return &messageMap##theClass; } \
-	const GFX_MSGMAP_ENTRY* theClass :: GetThisMessageEntries() \
-	{ \
-		typedef theClass ThisClass;						   \
-		typedef baseClass TheBaseClass;					   \
-		static const GFX_MSGMAP_ENTRY _messageEntries[] =  \
-		{
+    const GFX_MSGMAP theClass::messageMap##theClass = { &baseClass::messageMap##baseClass, GetThisMessageEntries()};\
+    const GFX_MSGMAP* theClass :: GetMessageMap() const \
+        { return &messageMap##theClass; } \
+    const GFX_MSGMAP_ENTRY* theClass :: GetThisMessageEntries() \
+    { \
+        typedef theClass ThisClass;                           \
+        typedef baseClass TheBaseClass;                       \
+        static const GFX_MSGMAP_ENTRY _messageEntries[] =  \
+        {
 
 #define GFX_END_MESSAGE_MAP() \
-		{0, 0, 0, 0, 0, 0, (GFX_PMSG)0, 0, 0, 0 } \
-	}; \
-		return &_messageEntries[0]; \
-	}
+        {0, 0, 0, 0, 0, 0, (GFX_PMSG)0, 0, 0, 0 } \
+    }; \
+        return &_messageEntries[0]; \
+    }
 
 #define GFX_EMPTY_MESSAGE_MAP(theClass) \
-	const BS_MSGMAP* theClass::GetMessageMap() const \
-		{ return &messageMap##theClass; } \
-	const BS_MSGMAP_ENTRY* theClass :: GetThisMessageEntries() \
-	{ \
-		typedef theClass ThisClass;						   \
-		static const BS_MSGMAP_ENTRY _messageEntries[] =  \
-		{ \
-		{0/*cmd*/, 0/*code*/, 0/*ID*/, 0/*LastID*/, 0/*nSig*/, 0/*nU0*/, (BS_PMSG)0/*pfn*/, 0/*nU1*/, 0/*nU2*/, 0/*nU3*/ } \
-	}; \
-		return &_messageEntries[0]; \
-	} \
-	const BS_MSGMAP theClass::messageMap##theClass = {NULL , GetThisMessageEntries()};
+    const BS_MSGMAP* theClass::GetMessageMap() const \
+        { return &messageMap##theClass; } \
+    const BS_MSGMAP_ENTRY* theClass :: GetThisMessageEntries() \
+    { \
+        typedef theClass ThisClass;                           \
+        static const BS_MSGMAP_ENTRY _messageEntries[] =  \
+        { \
+        {0/*cmd*/, 0/*code*/, 0/*ID*/, 0/*LastID*/, 0/*nSig*/, 0/*nU0*/, (BS_PMSG)0/*pfn*/, 0/*nU1*/, 0/*nU2*/, 0/*nU3*/ } \
+    }; \
+        return &_messageEntries[0]; \
+    } \
+    const BS_MSGMAP theClass::messageMap##theClass = {NULL , GetThisMessageEntries()};
 
