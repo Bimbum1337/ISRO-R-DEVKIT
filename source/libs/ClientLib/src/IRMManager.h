caf0c823a067202e9be6296e0c43e2b2d0fea443
florian0 <florian0@protonmail.com>
2020-03-18 20:23:34 +0100

    Document and format CIRMManager

--- source/libs/ClientLib/src/IRMManager.h
+++ source/libs/ClientLib/src/IRMManager.h
@@ -1,32 +1,44 @@
 #pragma once
-#pragma once
+
 #include <string>
 #include <BSLib/BSLib.h>
+
+// Resolve a circular dependency
 class CIFWnd;
-class CIRMManager;
 
-class CIRMManager
-{
-	
+/// Loader for resinfo/*.txt files
+class CIRMManager {
 public:
-	CIRMManager();
+    CIRMManager();
 
-	void LoadFromFile(const char *filename);
-	void CreateInterfaceSection(std::string section, CObj* base);
-	CIFWnd* GetResObj(int id, int a2);
+    /// Load a resinfo file from the PK2
+    /// \param filename Path and filename in Media.pk2 to load. Example: "resinfo/file.txt"
+    void LoadFromFile(const char *filename);
 
-	template<typename T>
-	T* GetResObj(int id, int a2);
+    /// Instantiate all controls in the named section
+    /// \param section Name of the section to load. Example: "Create"
+    /// \param base Pointer to the object to instantiate the controls in.
+    void CreateInterfaceSection(std::string section, CObj *base);
+
+    /// Get a pointer to a control identified by the Id
+    /// \param id The Id of the control as stated in the resinfo file or set on creation
+    /// \param a2 Completely unknown, is always 1
+    /// \returns Pointer to the control, NULL if Id was not found.
+    CIFWnd *GetResObj(int id, int a2);
+
+    /// Get a pointer to a control identified by the Id
+    /// \tparam T Expected type of the control. The type is never validated.
+    /// \param id The Id of the control as stated in the resinfo file or set on creation
+    /// \param a2 Completely unknown, is always 1
+    /// \returns Pointer to the control, NULL if Id was not found.
+    template<typename T>
+    T *GetResObj(int id, int a2) {
+        return reinterpret_cast<T *(__thiscall *)(CIRMManager *, int, int)>(0x008B51F0)(this, id, a2);
+    }
 
 private:
-	std::n_map<int, void*> m_interfacemap;
-	char _gap0[4];
-	std::n_string m_str;
-	int N00009CBC;
+    std::n_map<int, void *> m_interfacemap;
+    char _gap0[4];
+    std::n_string m_str;
+    int N00009CBC;
 };
-
-template <typename T>
-T* CIRMManager::GetResObj(int id, int a2)
-{
-	return reinterpret_cast<T*>(GetResObj(id, a2));
-}
