701279206913dc7c910d8ebe5ae18964b45fa717
florian0 <florian0@protonmail.com>
2020-03-18 09:20:49 +0100

    Reformat BSMsg

--- source/libs/BSLib/src/BSLib/_internal/_BSMsg.h
+++ source/libs/BSLib/src/BSLib/_internal/_BSMsg.h
@@ -7,68 +7,67 @@
 #endif
 
 /// Supported function signatures for message map handlers
-enum BSSig
-{
+enum BSSig {
     /// Mark the end of the message map array
-	GfxSig_end = 0,
-	BSSig_u10 = 10, // int (int, int)
-	BSSig_u12 = 12, //
-	BSSig_u16 = 16  // int (UINT, UINT, UINT)
+            GfxSig_end = 0,
+    BSSig_u10 = 10, // int (int, int)
+    BSSig_u12 = 12, //
+    BSSig_u16 = 16  // int (UINT, UINT, UINT)
 };
 
 #define GFX_WM_COMMAND 0x0001
 
 #define ONG_COMMAND(Id, Func) \
-	{ GFX_WM_COMMAND , 0, Id, Id, BSSig_u12, 0, \
-		(GFX_PMSG) \
-		(static_cast< void (GFX_MSG_CALL CGWndBase::*)() > ( Func )), \
-		0, 0, 0 },
+    { GFX_WM_COMMAND , 0, Id, Id, BSSig_u12, 0, \
+        (GFX_PMSG) \
+        (static_cast< void (GFX_MSG_CALL CGWndBase::*)() > ( Func )), \
+        0, 0, 0 },
 
 
 #define ONG_CHAR() \
-	{ 258, 0, 0, 0, BSSig_u16, 0, \
-		(GFX_PMSG) \
-		(static_cast< int (GFX_MSG_CALL CGWndBase::*)(UINT, UINT, UINT) > ( &ThisClass :: OnChar)), \
-		0, 0, 0},
+    { 258, 0, 0, 0, BSSig_u16, 0, \
+        (GFX_PMSG) \
+        (static_cast< int (GFX_MSG_CALL CGWndBase::*)(UINT, UINT, UINT) > ( &ThisClass :: OnChar)), \
+        0, 0, 0},
 
 #define ONG_WM_32868() \
-	{ 32868, 0, 0, 0, BSSig_u10, 0, \
-	(GFX_PMSG) \
-	(static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On32868)), \
-		0, 0, 0},
+    { 32868, 0, 0, 0, BSSig_u10, 0, \
+    (GFX_PMSG) \
+    (static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On32868)), \
+        0, 0, 0},
 
 #define ONG_WM_4001() \
-	{ 0x4001, 0, 0, 0, 0xA, 0, \
-	(GFX_PMSG) \
-	(static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4001)), \
-		0, 0, 0},
+    { 0x4001, 0, 0, 0, 0xA, 0, \
+    (GFX_PMSG) \
+    (static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4001)), \
+        0, 0, 0},
 
 #define ONG_WM_4002() \
-	{ 0x4002, 0, 0, 0, 0xA, 0, \
-	(GFX_PMSG) \
-	(static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4002)), \
-		0, 0, 0},
+    { 0x4002, 0, 0, 0, 0xA, 0, \
+    (GFX_PMSG) \
+    (static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4002)), \
+        0, 0, 0},
 
 #define ONG_WM_4003() \
-	{ 0x4003, 0, 0, 0, 0xA, 0, \
-	(GFX_PMSG) \
-	(static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4003)), \
-		0, 0, 0},
+    { 0x4003, 0, 0, 0, 0xA, 0, \
+    (GFX_PMSG) \
+    (static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4003)), \
+        0, 0, 0},
 
 #define ONG_VISIBLE_CHANGE() \
-	{ 0x4004, 0, 0, 0, 0xA, 0, \
-	(GFX_PMSG) \
-	(static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: OnVisibleStateChange)), \
-	0, 0, 0},
+    { 0x4004, 0, 0, 0, 0xA, 0, \
+    (GFX_PMSG) \
+    (static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: OnVisibleStateChange)), \
+    0, 0, 0},
 
 #define ONG_WM_4005() \
-	{ 0x4005, 0, 0, 0, 0xA, 0, \
-	(GFX_PMSG) \
-	(static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4005)), \
-		0, 0, 0},
+    { 0x4005, 0, 0, 0, 0xA, 0, \
+    (GFX_PMSG) \
+    (static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4005)), \
+        0, 0, 0},
 
 #define ONG_WM_4006() \
-	{ 0x4001, 0, 0, 0, 0xA, 0, \
-	(GFX_PMSG) \
-	(static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4006)), \
-		0, 0, 0},
+    { 0x4001, 0, 0, 0, 0xA, 0, \
+    (GFX_PMSG) \
+    (static_cast< int (GFX_MSG_CALL CGWndBase::*)(int, int) > ( &ThisClass :: On4006)), \
+        0, 0, 0},
