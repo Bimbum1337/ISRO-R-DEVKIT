d9817927e7dea2346f38c64902214874706decf6
florian0 <florian0@protonmail.com>
2020-03-19 21:41:54 +0100

    Reformat CGWnd

--- source/libs/ClientLib/src/GWnd.cpp
+++ source/libs/ClientLib/src/GWnd.cpp
@@ -3,117 +3,96 @@
 
 GFX_IMPLEMENT_DYNAMIC_EXISTING(CGWnd, 0x0110F94C)
 
-void CGWnd::F24(int a1, int a2)
-{
-	reinterpret_cast<void(__thiscall*)(CGWnd*,int,int)>(0x00B9D810)(this, a1, a2);
+void CGWnd::F24(int a1, int a2) {
+    reinterpret_cast<void (__thiscall *)(CGWnd *, int, int)>(0x00B9D810)(this, a1, a2);
 }
 
-bool CGWnd::F25(int a1)
-{
-	return true;
+bool CGWnd::F25(int a1) {
+    return true;
 }
 
-void CGWnd::F26(int a1)
-{
+void CGWnd::F26(int a1) {
 }
 
-bool CGWnd::F27()
-{
-	return true;
+bool CGWnd::F27() {
+    return true;
 }
 
-bool CGWnd::F28(int a1, int a2, int a3)
-{
-	return false;
+bool CGWnd::F28(int a1, int a2, int a3) {
+    return false;
 }
 
-int CGWnd::OnClickEvent(int a1, int x, int y)
-{
-	return 0;
+int CGWnd::OnClickEvent(int a1, int x, int y) {
+    return 0;
 }
 
-int CGWnd::F30(int a1, int a2, int a3)
-{
-	return 0;
+int CGWnd::F30(int a1, int a2, int a3) {
+    return 0;
 }
 
-int CGWnd::F31(int a1, int a2, int a3)
-{
-	return 0;
+int CGWnd::F31(int a1, int a2, int a3) {
+    return 0;
 }
 
-int CGWnd::F32(int a1, int a2, int a3)
-{
-	return 0;
+int CGWnd::F32(int a1, int a2, int a3) {
+    return 0;
 }
 
-int CGWnd::F33(int a1, int a2, int a3)
-{
-	return 0;
+int CGWnd::F33(int a1, int a2, int a3) {
+    return 0;
 }
 
-int CGWnd::F34(int a1, int a2, int a3)
-{
-	return 0;
+int CGWnd::F34(int a1, int a2, int a3) {
+    return 0;
 }
 
-int CGWnd::F35(int a1, int a2, int a3)
-{
-	return 0;
+int CGWnd::F35(int a1, int a2, int a3) {
+    return 0;
 }
 
-int CGWnd::F36(int a1, int a2, int a3, int a4)
-{
-	return 0;
+int CGWnd::F36(int a1, int a2, int a3, int a4) {
+    return 0;
 }
 
-void CGWnd::Func_37()
-{
-	return;
+void CGWnd::Func_37() {
+    return;
 }
 
-CGWnd::CGWnd()
-{
-	m_bVisible = true;
-	m_bClickable = true;
-	N0000074F = true;
-	N00000702 = false;
+CGWnd::CGWnd() {
+    m_bVisible = true;
+    m_bClickable = true;
+    N0000074F = true;
+    N00000702 = false;
 
-	sub_B9DA70(false);
+    sub_B9DA70(false);
 }
 
-void CGWnd::OnUpdate()
-{
-	// empty
+void CGWnd::OnUpdate() {
+    // empty
 }
 
-void CGWnd::RenderMyself()
-{
-	if (!m_bVisible)
-		return;
-	// NOTE: This can trigger an endless recursion if RenderMyself is not properly overwritten
-	CGWnd::Render();
+void CGWnd::RenderMyself() {
+    if (!m_bVisible)
+        return;
+    // NOTE: This can trigger an endless recursion if RenderMyself is not properly overwritten
+    CGWnd::Render();
 }
 
-void CGWnd::RenderMyChildren()
-{
-	reinterpret_cast<void(__thiscall*)(CGWnd*)>(0x00BA0300)(this);
+void CGWnd::RenderMyChildren() {
+    reinterpret_cast<void (__thiscall *)(CGWnd *)>(0x00BA0300)(this);
 }
 
-void CGWnd::Render()
-{
-	RenderMyself();
-	RenderMyChildren();
-	Func_15();
+void CGWnd::Render() {
+    RenderMyself();
+    RenderMyChildren();
+    Func_15();
 }
 
-void CGWnd::Func_15()
-{
-	// empty
+void CGWnd::Func_15() {
+    // empty
 }
 
-bool CGWnd::On3DEvent_MAYBE(Event3D* a2)
-{
-	return false;
+bool CGWnd::On3DEvent_MAYBE(Event3D *a2) {
+    return false;
 }
 
--- source/libs/ClientLib/src/GWnd.h
+++ source/libs/ClientLib/src/GWnd.h
@@ -6,57 +6,69 @@ class CProcess; /* forward decl to avoid include recursion */
 
 /// Adds just a bunch of virtuals to \ref CGWndBase which are pretty much all unknown ...
 class CGWnd : public CGWndBase {
-	// 132 - 132 = 0 <- no additional bytes :)
-	GFX_DECLARE_DYNAMIC_EXISTING(CGWnd, 0x0110F94C)
+    // 132 - 132 = 0 <- no additional bytes :)
+    GFX_DECLARE_DYNAMIC_EXISTING(CGWnd, 0x0110F94C)
 
 public:
     /// \address 00B9D810
-	virtual void F24(int a1, int a2);
-	virtual bool F25(int a1);
-	virtual void F26(int a1);
-	virtual bool F27();
-	virtual bool F28(int a1, int a2, int a3);
-	virtual int OnClickEvent(int a1, int x, int y);
-	virtual int F30(int a1, int a2, int a3);
-	virtual int F31(int a1, int a2, int a3);
-	virtual int F32(int a1, int a2, int a3);
-	virtual int F33(int a1, int a2, int a3);
-	virtual int F34(int a1, int a2, int a3);
-	virtual int F35(int a1, int a2, int a3);
-	virtual int F36(int a1, int a2, int a3, int a4);
-	virtual void Func_37();
+    virtual void F24(int a1, int a2);
 
-	/// \address 00B9E270
-	CGWnd();
+    virtual bool F25(int a1);
 
-	/// \address 00B9D7E0
-	void OnUpdate() override;
+    virtual void F26(int a1);
 
-	/// \address 00B9D850
-	void RenderMyself() override;
+    virtual bool F27();
 
-	/// \address 00BA0300
-	void RenderMyChildren() override;
+    virtual bool F28(int a1, int a2, int a3);
 
-	/// \address 00B9D7F0
-	void Render() override;
+    virtual int OnClickEvent(int a1, int x, int y);
 
-	/// \address 00B9D880
-	void Func_15() override;
+    virtual int F30(int a1, int a2, int a3);
 
-	/// \address 00B9D890
-	bool On3DEvent_MAYBE(Event3D* a2) override;
+    virtual int F31(int a1, int a2, int a3);
+
+    virtual int F32(int a1, int a2, int a3);
+
+    virtual int F33(int a1, int a2, int a3);
+
+    virtual int F34(int a1, int a2, int a3);
+
+    virtual int F35(int a1, int a2, int a3);
+
+    virtual int F36(int a1, int a2, int a3, int a4);
+
+    virtual void Func_37();
+
+    /// \address 00B9E270
+    CGWnd();
+
+    /// \address 00B9D7E0
+    void OnUpdate() override;
+
+    /// \address 00B9D850
+    void RenderMyself() override;
+
+    /// \address 00BA0300
+    void RenderMyChildren() override;
+
+    /// \address 00B9D7F0
+    void Render() override;
+
+    /// \address 00B9D880
+    void Func_15() override;
+
+    /// \address 00B9D890
+    bool On3DEvent_MAYBE(Event3D *a2) override;
 
 
-
-	/// Create a new CGWnd-instance based on the given runtime class
-	/// \param pParent Parent control this control should live in
-	/// \param pType Runtime class type identifying the class to be created
-	/// \param Rect Initial dimension and position
-	/// \param a4 Presumably the Id
-	/// \param a5 Unknown
-	static CGWnd* CreateInstance(CGWnd* pParent, const CGfxRuntimeClass& pType, RECT& Rect, int a4, int a5)
-	{
-		return reinterpret_cast<CGWnd*(__cdecl*)(CGWnd*,const CGfxRuntimeClass*,RECT*,int,int)>(0x00BA20B0)(pParent, &pType, &Rect, a4, a5);
-	}
+    /// Create a new CGWnd-instance based on the given runtime class
+    /// \param pParent Parent control this control should live in
+    /// \param pType Runtime class type identifying the class to be created
+    /// \param Rect Initial dimension and position
+    /// \param a4 Presumably the Id
+    /// \param a5 Unknown
+    static CGWnd *CreateInstance(CGWnd *pParent, const CGfxRuntimeClass &pType, RECT &Rect, int a4, int a5) {
+        return reinterpret_cast<CGWnd *(__cdecl *)(CGWnd *, const CGfxRuntimeClass *, RECT *, int, int)>(0x00BA20B0)(
+                pParent, &pType, &Rect, a4, a5);
+    }
 };
