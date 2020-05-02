bbf410afcf214f6907b88630a9b0d34c783ba2af
florian0 <florian0@protonmail.com>
2020-05-02 15:56:44 +0200

    Test fixture of CIEntity

--- source/libs/GFXMainFrame/src/GFXMainFrame/IEntity.h
+++ source/libs/GFXMainFrame/src/GFXMainFrame/IEntity.h
@@ -6,4 +6,10 @@
 class CIEntity : public CObjChild {
 public:
     char pad_0020[52]; //0x0020
-}; //Size: 0x0054
+
+    BEGIN_FIXTURE()
+        ENSURE_SIZE(0x54)
+    END_FIXTURE()
+
+    RUN_FIXTURE(CIEntity)
+};
