3efcb4aee79336fc7324721acd6015c7accb3301
florian0 <florian0@protonmail.com>
2020-05-02 15:54:58 +0200

    Reformat CIEntity

--- source/libs/GFXMainFrame/src/GFXMainFrame/IEntity.h
+++ source/libs/GFXMainFrame/src/GFXMainFrame/IEntity.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <BSLib/BSLib.h>
+#include "Test/Test.h"
 
-class CIEntity : public CObjChild
-{
+class CIEntity : public CObjChild {
 public:
-	char pad_0020[52]; //0x0020
+    char pad_0020[52]; //0x0020
 }; //Size: 0x0054
