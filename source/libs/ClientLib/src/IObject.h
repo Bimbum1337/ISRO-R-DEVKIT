fc1e32b06e6243e52d27598c02b8aad09782a346
florian0 <florian0@protonmail.com>
2020-05-02 15:57:14 +0200

    Reformat CIObject

--- source/libs/ClientLib/src/IObject.h
+++ source/libs/ClientLib/src/IObject.h
@@ -7,13 +7,12 @@
 #include "GlobalHelpersThatHaveNoHomeYet.h"
 
 
-class CIObject : public CIEntity
-{
+class CIObject : public CIEntity {
 public:
-	char pad_0054[40]; //0x0054
-	uregion region; //0x007C
-	char pad_007E[2]; //0x007E
-	D3DVECTOR location; //0x0080
-	float rotation; //0x008C
-	char pad_0090[76]; //0x0090
+    char pad_0054[40]; //0x0054
+    uregion region; //0x007C
+    char pad_007E[2]; //0x007E
+    D3DVECTOR location; //0x0080
+    float rotation; //0x008C
+    char pad_0090[76]; //0x0090
 }; //Size: 0x00DC
