c1b49f76e86cbd102f1232170e17058f16497b31
florian0 <florian0@protonmail.com>
2020-07-10 12:02:24 +0200

    Refactor CIFFrame
    - Reformat to current project settings
    - Add test fixture for size
    - Redirect OnCreate to original function

--- source/libs/ClientLib/src/IFFrame.h
+++ source/libs/ClientLib/src/IFFrame.h
@@ -1,18 +1,37 @@
 #pragma once
+
 #include "IFTileWnd.h"
 
-class CIFFrame : public CIFTileWnd
-{
-	GFX_DECLARE_DYNAMIC_EXISTING(CIFFrame, 0x00EECB14)
-public:
-	CIFFrame(void);
-	~CIFFrame(void);
+class CIFFrame : public CIFTileWnd {
+    GFX_DECLARE_DYNAMIC_EXISTING(CIFFrame, 0x00EECB14)
 
-	bool OnCreate(long ln) override;
-	void RenderMyself() override;
-	void Func_49(std::n_string str) override;
-	
+public:
+    // Constructor is trivial
+    // \address 008151E0
+    // CIFFrame();
+
+    // Destructor is trivial
+    // \address 00815210
+    // ~CIFFrame();
+
+    /// \address 00815230
+    bool OnCreate(long ln) override;
+
+    /// \address 00815490
+    void RenderMyself() override;
+
+    /// \address 00815300
+    void Func_49(std::n_string str) override;
+
 private:
-	// No members
+    // No members
+
+
+    BEGIN_FIXTURE()
+        ENSURE_SIZE(0x7b0)
+    END_FIXTURE()
+    RUN_FIXTURE(CIFFrame)
+
+
 };
 
