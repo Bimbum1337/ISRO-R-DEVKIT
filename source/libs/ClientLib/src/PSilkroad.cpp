670bb5ac86796705178cba604c45468133b02658
florian0 <florian0@protonmail.com>
2020-08-15 01:21:39 +0200

    Cleanup CPSilkroad

--- source/libs/ClientLib/src/PSilkroad.cpp
+++ source/libs/ClientLib/src/PSilkroad.cpp
@@ -1,16 +1,14 @@
-#include "StdAfx.h"
 #include "PSilkroad.h"
 
-void CPSilkroad::Func_40()
-{
+void CPSilkroad::Func_40() {
+    // empty
 }
 
-void CPSilkroad::ErrorMessage(const wchar_t* message)
-{
-	reinterpret_cast<void(__thiscall*)(CPSilkroad*,const wchar_t*)>(0x00861850)(this, message);
+void CPSilkroad::ErrorMessage(const wchar_t *message) {
+    reinterpret_cast<void (__thiscall *)(CPSilkroad *, const wchar_t *)>(0x00861850)(this, message);
 }
 
-void CPSilkroad::ErrorMessagePar(const wchar_t* message, char codeletter, int codenumber)
-{
-	reinterpret_cast<void(__thiscall*)(CPSilkroad*,const wchar_t*,char,int)>(0x00861890)(this, message, codeletter, codenumber);
+void CPSilkroad::ErrorMessagePar(const wchar_t *message, char codeletter, int codenumber) {
+    reinterpret_cast<void (__thiscall *)(CPSilkroad *, const wchar_t *, char, int)>(0x00861890)(this, message,
+                                                                                                codeletter, codenumber);
 }
--- source/libs/ClientLib/src/PSilkroad.h
+++ source/libs/ClientLib/src/PSilkroad.h
@@ -1,16 +1,18 @@
 #pragma once
 
-#include "IRMManager.h"
 #include <GFXMainFrame/Process.h>
-
+#include "IRMManager.h"
 
 class CPSilkroad : public CProcess {
 public:
 
+    /// \address 00849c20
     virtual void Func_40();
 
+    /// \address 00861850
     void ErrorMessage(const wchar_t *message);
 
+    /// \address 00861890
     void ErrorMessagePar(const wchar_t *message, char codeletter, int codenumber);
 
 
@@ -21,6 +23,7 @@ protected:
 
     BEGIN_FIXTURE()
         ENSURE_SIZE(0xe0)
+        ENSURE_OFFSET(m_IRM, 0xB0)
     END_FIXTURE()
 
     RUN_FIXTURE(CPSilkroad)
