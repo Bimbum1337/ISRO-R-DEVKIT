b9abe3798f9d038a7a2c60ee253f8fb73b611a49
florian0 <florian0@protonmail.com>
2020-08-25 22:48:23 +0200

    Refactor QuickStart-Hooks
    The code is now a lot cleaner and easier to understand.

--- source/DevKit_DLL/src/QuickStart.h
+++ source/DevKit_DLL/src/QuickStart.h
@@ -5,17 +5,27 @@
 class QuickStart {
 public:
     void Setup();
+
     void LoadConfig();
+
     void PlaceHooks();
 
-    bool IsEnabled();
+    bool IsEnabled() const;
 
-    void PreWinMain();
+    QuickStart();
 
 private:
+    void ConfigReadA(const char *key, char *value, size_t nSize) const;
+
+    void ConfigReadW(const wchar_t *key, wchar_t *value, size_t nSize) const;
+
+private:
+    const char *cPath;
+    const wchar_t *wPath;
+
+    bool m_enabled;
     wchar_t servername[32];
     wchar_t userid[32];
     wchar_t passwd[32];
-    char CPSQuickStartEnabled[2];
 };
 
