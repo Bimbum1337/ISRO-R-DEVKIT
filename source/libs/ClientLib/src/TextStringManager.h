b53fb467360c7c6aa56301b8aa1282c8c67b8be9
florian0 <florian0@protonmail.com>
2020-10-26 07:40:56 +0100

    Fix typo in expansion of TSM_GETTEXT-macro

--- source/libs/ClientLib/src/TextStringManager.h
+++ source/libs/ClientLib/src/TextStringManager.h
@@ -22,4 +22,4 @@ public:
 #define g_CTextStringManager ((CTextStringManager*)0x00EEDE90)
 
 #define TSM_GETTEXTPTR(text) (g_CTextStringManager->GetString(L##text)->c_str())
-#define TSM_GETTEXT(text) (g_CTexStringManager->GetString(L##text))
+#define TSM_GETTEXT(text) (g_CTextStringManager->GetString(L##text))
