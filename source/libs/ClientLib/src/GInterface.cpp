d6d04a1f813f06c6fe4c06dd002a4a16486c0a0c
florian0 <florian0@protonmail.com>
2020-11-14 20:51:21 +0100

    Reformat and cleanup CGInterface
    CGInterface is a mess. Spaces and tabs mixed, missing comments,
    Since I'm touching CGInterface in this branch, I'll clean it up first.

--- source/libs/ClientLib/src/GInterface.cpp
+++ source/libs/ClientLib/src/GInterface.cpp
@@ -5,48 +5,44 @@
 #include "IFChatViewer.h"
 #include "IFNotify.h"
 
-void CGInterface::RenderToggle_GDR_ACTION()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079D5B0)(this);
+void CGInterface::RenderToggle_GDR_ACTION() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079D5B0)(this);
 }
 
-void CGInterface::RenderToggle_GDR_APPRENTICESHIP()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079B0B0)(this);
-}
-void CGInterface::RenderToggle_GDR_PLAYERINFO()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079ACE0)(this);
+void CGInterface::RenderToggle_GDR_APPRENTICESHIP() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079B0B0)(this);
 }
 
-void CGInterface::RenderToggle_GDR_GAMEGUIDE()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079F690)(this);
-}
-void CGInterface::RenderToggle_GDR_INVENTORY()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079B020)(this);
-}
-void CGInterface::RenderToggle_GDR_PARTY()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079AE90)(this);
-}
-void CGInterface::RenderToggle_GDR_SKILL()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079AE00)(this);
+void CGInterface::RenderToggle_GDR_PLAYERINFO() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079ACE0)(this);
 }
 
-void CGInterface::RenderToggle_WORLDMAP_GUIDE()
-{
-	return reinterpret_cast<void(__thiscall*)(void*)>(0x0079ABE0)(this);
+void CGInterface::RenderToggle_GDR_GAMEGUIDE() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079F690)(this);
 }
-void CGInterface::Render_GDR_AUTO_POTION(bool visible)
-{
-	return reinterpret_cast<void(__thiscall*)(void*, bool)>(0x0079C750)(this, visible);
+
+void CGInterface::RenderToggle_GDR_INVENTORY() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079B020)(this);
+}
+
+void CGInterface::RenderToggle_GDR_PARTY() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079AE90)(this);
+}
+
+void CGInterface::RenderToggle_GDR_SKILL() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079AE00)(this);
+}
+
+void CGInterface::RenderToggle_WORLDMAP_GUIDE() {
+    return reinterpret_cast<void (__thiscall *)(void *)>(0x0079ABE0)(this);
+}
+
+void CGInterface::Render_GDR_AUTO_POTION(bool visible) {
+    return reinterpret_cast<void (__thiscall *)(void *, bool)>(0x0079C750)(this, visible);
 }
 
 void CGInterface::CreateFlorian0Event() {
-	CGWnd *guide = GetAlarmManager()->GetGuide(GUIDE_FLORIAN0);
+    CGWnd *guide = GetAlarmManager()->GetGuide(GUIDE_FLORIAN0);
 }
 
 CAlramGuideMgrWnd *CGInterface::GetAlarmManager() {
@@ -57,7 +53,7 @@ bool CGInterface::OnCreateIMPL(long ln) {
 
     BeforeOnCreate();
 
-    bool b = reinterpret_cast<bool(__thiscall*)(CGInterface*, long)>(0x0078c910)(this, ln);
+    bool b = reinterpret_cast<bool (__thiscall *)(CGInterface *, long)>(0x0078c910)(this, ln);
 
     AfterOnCreate();
 
@@ -73,88 +69,81 @@ void CGInterface::AfterOnCreate() {
 }
 
 void CGInterface::ShowMessage_Quest(const std::n_wstring &msg) {
-    CIFNotify* notify = m_IRM.GetResObj<CIFNotify>(GDR_UPDATE_QUEST_INFO, 1);
+    CIFNotify *notify = m_IRM.GetResObj<CIFNotify>(GDR_UPDATE_QUEST_INFO, 1);
     notify->ShowMessage(msg);
 }
 
 void CGInterface::ShowMessage_Notice(const std::n_wstring &msg) {
-    CIFNotify* notify = m_IRM.GetResObj<CIFNotify>(GDR_NOTICE, 1);
+    CIFNotify *notify = m_IRM.GetResObj<CIFNotify>(GDR_NOTICE, 1);
     notify->ShowMessage(msg);
 }
 
 void CGInterface::ShowMessage_Warning(const std::n_wstring &msg) {
-    CIFNotify* notify = m_IRM.GetResObj<CIFNotify>(GDR_WARNING_WND, 1);
+    CIFNotify *notify = m_IRM.GetResObj<CIFNotify>(GDR_WARNING_WND, 1);
     notify->ShowMessage(msg);
 }
 
 
-int CGInterface::Get_SelectedObjectId()
-{
-	return this->m_selectedObjectId;
+int CGInterface::Get_SelectedObjectId() {
+    return this->m_selectedObjectId;
 }
 
-CIFTimerWnd* CGInterface::Get_TimerWindow()
-{
-	return this->m_timerWindow;
+CIFTimerWnd *CGInterface::Get_TimerWindow() {
+    return this->m_timerWindow;
 }
 
-CIFQuickStateHalfWnd* CGInterface::Get_QuickStateHalfWnd()
-{
-	return this->N00002637;
+CIFQuickStateHalfWnd *CGInterface::Get_QuickStateHalfWnd() {
+    return this->N00002637;
 }
 
-CIF_NPCWindow* CGInterface::Get_GDR_NPCWINDOW()
-{
-	return (CIF_NPCWindow*)this->m_IRM.GetResObj(GDR_NPCWINDOW, 1);
+CIF_NPCWindow *CGInterface::Get_GDR_NPCWINDOW() {
+    return (CIF_NPCWindow *) this->m_IRM.GetResObj(GDR_NPCWINDOW, 1);
 }
 
-CIFMainPopup* CGInterface::Get_GDR_MAINPOPUP()
-{
-	return (CIFMainPopup*)this->m_IRM.GetResObj(GDR_MAINPOPUP, 1);
+CIFMainPopup *CGInterface::Get_GDR_MAINPOPUP() {
+    return (CIFMainPopup *) this->m_IRM.GetResObj(GDR_MAINPOPUP, 1);
 }
 
-CIFStorageRoom* CGInterface::Get_GDR_STORAGEROOM()
-{
-	return (CIFStorageRoom*)this->m_IRM.GetResObj(GDR_STORAGEROOM, 1);
+CIFStorageRoom *CGInterface::Get_GDR_STORAGEROOM() {
+    return (CIFStorageRoom *) this->m_IRM.GetResObj(GDR_STORAGEROOM, 1);
 }
 
-CIFStorageRoom* CGInterface::Get_GDR_GUILDSTORAGEROOM()
-{
-	return (CIFStorageRoom*)this->m_IRM.GetResObj(GDR_GUILDSTORAGEROOM, 1);
+CIFStorageRoom *CGInterface::Get_GDR_GUILDSTORAGEROOM() {
+    return (CIFStorageRoom *) this->m_IRM.GetResObj(GDR_GUILDSTORAGEROOM, 1);
 }
 
-void CGInterface::WriteErrorMessage(byte errorType, unsigned __int16 errorCode, int colorARGB, int a5, int a6)
-{
-	return reinterpret_cast<void(__thiscall*)(void*, byte, unsigned __int16, int, int, int)>(0x00778190)(this, errorType, errorCode, colorARGB, a5, a6);
+void CGInterface::WriteErrorMessage(byte errorType, unsigned __int16 errorCode, int colorARGB, int a5, int a6) {
+    return reinterpret_cast<void (__thiscall *)(void *, byte, unsigned __int16, int, int, int)>(0x00778190)(this, errorType, errorCode, colorARGB, a5, a6);
 }
 
 void CGInterface::WriteSystemMessage(eLogType level, const wchar_t *str) {
-    reinterpret_cast<void(__thiscall*)(CGInterface*,eLogType,const wchar_t*)>(0x007781B0)(this, level, str);
+    reinterpret_cast<void (__thiscall *)(CGInterface *, eLogType, const wchar_t *)>(0x007781B0)(this, level, str);
 }
 
 void CGInterface::sub_787C10(SChatMetaData &meta) {
-    reinterpret_cast<void(__thiscall*)(CGInterface*, SChatMetaData*)>(0x787C10)(this, &meta);
+    reinterpret_cast<void (__thiscall *)(CGInterface *, SChatMetaData *)>(0x787C10)(this, &meta);
 }
 
 int CGInterface::TryParseChatCommands(const wchar_t *text, RECT &r, std::vector<void *> &v) {
-    return reinterpret_cast<int(__thiscall*)(CGInterface*, const wchar_t *, RECT*, std::vector<void*> *)>(0x0078BEA0)(this, text, &r, &v);
+    return reinterpret_cast<int (__thiscall *)(CGInterface *, const wchar_t *, RECT *,
+                                               std::vector<void *> *)>(0x0078BEA0)(this, text, &r, &v);
 }
 
 CNIFCommunityWnd *CGInterface::GetCommunityWnd() {
-    return reinterpret_cast<CNIFCommunityWnd*(__thiscall*)(CGInterface*)>(0x007994f0)(this);
+    return reinterpret_cast<CNIFCommunityWnd *(__thiscall *)(CGInterface *)>(0x007994f0)(this);
 }
 
 void CGInterface::FUN_00777c30(ChatType type, const wchar_t *message, D3DCOLOR color, int a5) {
-    CIFChatViewer* chatViewer = m_IRM.GetResObj<CIFChatViewer>(GDR_CHATVIEWER, 1);
+    CIFChatViewer *chatViewer = m_IRM.GetResObj<CIFChatViewer>(GDR_CHATVIEWER, 1);
     chatViewer->FUN_007aca30(type, color, message, 0, a5);
 }
 
-void CGInterface::FUN_00778a10(int a2, const wchar_t* message, D3DCOLOR color) {
-    reinterpret_cast<void(__thiscall*)(CGInterface*, int, const wchar_t*, D3DCOLOR)>(0x00778a10)(this, a2, message, color);
+void CGInterface::FUN_00778a10(int a2, const wchar_t *message, D3DCOLOR color) {
+    reinterpret_cast<void (__thiscall *)(CGInterface *, int, const wchar_t *, D3DCOLOR)>(0x00778a10)(this, a2, message, color);
 }
 
 void CGInterface::FUN_00777cf0(const std::n_wstring &recipient) {
-    reinterpret_cast<void(__thiscall*)(CGInterface*, const std::n_wstring*)>(0x00777cf0)(this, &recipient);
+    reinterpret_cast<void (__thiscall *)(CGInterface *, const std::n_wstring *)>(0x00777cf0)(this, &recipient);
 }
 
 CIFSystemMessage *CGInterface::GetSystemMessageView() {
--- source/libs/ClientLib/src/GInterface.h
+++ source/libs/ClientLib/src/GInterface.h
@@ -29,11 +29,12 @@
 #define GDR_SYSTEM_MESSAGE_VIEW 68
 #define GDR_GUILDSTORAGEROOM 145
 
-enum eLogType : int
-{
-	SYSLOG_NONE = 0,
-	SYSLOG_ERR = 4,
-	SYSLOG_GUIDE = 6
+#define g_pCGInterface (*(CGInterface**)0x0110F80C)
+
+enum eLogType : int {
+    SYSLOG_NONE = 0,
+    SYSLOG_ERR = 4,
+    SYSLOG_GUIDE = 6
 };
 
 struct SChatMetaData {
@@ -56,13 +57,14 @@ struct SChatMetaData {
 class CGInterface : public CIFWnd {
 
 public:
-	// int OnKeyDown(int keycode, int a3, char a4);
+    // int OnKeyDown(int keycode, int a3, char a4);
 
-	// void ToggleHudVisibility();
+    // void ToggleHudVisibility();
 
-	bool OnCreateIMPL(long ln);
+    bool OnCreateIMPL(long ln);
 
-	void BeforeOnCreate();
+    void BeforeOnCreate();
+
     void AfterOnCreate();
 
 
@@ -112,79 +114,79 @@ public:
     void ShowMessage_Quest(const std::n_wstring &msg);
 
     /// \address 007781B0
-	void WriteSystemMessage(eLogType level, const wchar_t *str);
+    void WriteSystemMessage(eLogType level, const wchar_t *str);
 
     void CreateFlorian0Event();
 
-	CAlramGuideMgrWnd *GetAlarmManager();
+    CAlramGuideMgrWnd *GetAlarmManager();
 
-	//0x0079D5B0
-	void RenderToggle_GDR_ACTION();
+    /// \address 0079D5B0
+    void RenderToggle_GDR_ACTION();
 
-	//0x0079B0B0
-	void RenderToggle_GDR_APPRENTICESHIP();
+    /// \address 0079B0B0
+    void RenderToggle_GDR_APPRENTICESHIP();
 
-	//0x0079ACE0
-	void RenderToggle_GDR_PLAYERINFO();
+    /// \address 0079ACE0
+    void RenderToggle_GDR_PLAYERINFO();
 
-	//0x0079F690
-	void RenderToggle_GDR_GAMEGUIDE();
+    /// \address 0079F690
+    void RenderToggle_GDR_GAMEGUIDE();
 
-	//0x0079B020
-	void RenderToggle_GDR_INVENTORY();
+    /// \address 0079B020
+    void RenderToggle_GDR_INVENTORY();
 
-	//0x0079AE90
-	void RenderToggle_GDR_PARTY();
+    /// \address 0079AE90
+    void RenderToggle_GDR_PARTY();
 
-	//0x0079AE00
-	void RenderToggle_GDR_SKILL();
+    /// \address 0079AE00
+    void RenderToggle_GDR_SKILL();
 
-	//0x0079ABE0
-	void RenderToggle_WORLDMAP_GUIDE();
+    /// \address 0079ABE0
+    void RenderToggle_WORLDMAP_GUIDE();
 
-	//0x0079C750
-	void Render_GDR_AUTO_POTION(bool visible);
+    /// \address 0079C750
+    void Render_GDR_AUTO_POTION(bool visible);
 
-	//0x00778B70
-	int Get_SelectedObjectId();
+    /// \address 00778B70
+    int Get_SelectedObjectId();
 
-	//0x00777A30
-	CIFTimerWnd* Get_TimerWindow();
+    /// \address 00777A30
+    CIFTimerWnd *Get_TimerWindow();
 
-	//0x00777A50
-	CIFQuickStateHalfWnd* Get_QuickStateHalfWnd();
+    /// \address 00777A50
+    CIFQuickStateHalfWnd *Get_QuickStateHalfWnd();
 
-	//0x00798D40
-	CIF_NPCWindow* Get_GDR_NPCWINDOW();
+    /// \address 00798D40
+    CIF_NPCWindow *Get_GDR_NPCWINDOW();
 
-	//0x00798D00
-	CIFMainPopup* Get_GDR_MAINPOPUP();
+    /// \address 00798D00
+    CIFMainPopup *Get_GDR_MAINPOPUP();
 
-	//0x007994C0
-	CIFStorageRoom* Get_GDR_STORAGEROOM();
+    /// \address 007994C0
+    CIFStorageRoom *Get_GDR_STORAGEROOM();
 
-	//0x00799640
-	CIFStorageRoom* Get_GDR_GUILDSTORAGEROOM();
+    /// \address 00799640
+    CIFStorageRoom *Get_GDR_GUILDSTORAGEROOM();
 
-	/// \address 007994f0
-	CNIFCommunityWnd* GetCommunityWnd();
+    /// \address 007994f0
+    CNIFCommunityWnd *GetCommunityWnd();
 
-	CIFSystemMessage *GetSystemMessageView();
+    CIFSystemMessage *GetSystemMessageView();
 
-	/// \address 00777c30
-    void FUN_00777c30(ChatType type, const wchar_t* message, D3DCOLOR color, int a5);
+    /// \address 00777c30
+    void FUN_00777c30(ChatType type, const wchar_t *message, D3DCOLOR color, int a5);
 
     /// \address 00778a10
-    void FUN_00778a10(int a2, const wchar_t* message, D3DCOLOR color);
+    void FUN_00778a10(int a2, const wchar_t *message, D3DCOLOR color);
 
     /// \address 00777cf0
     void FUN_00777cf0(const std::n_wstring &recipient);
 
-	//0x00778190
-	void WriteErrorMessage(byte errorType, unsigned __int16 errorCode, int colorARGB, int a5, int a6);
+    /// \address 00778190
+    void WriteErrorMessage(byte errorType, unsigned __int16 errorCode, int colorARGB, int a5, int a6);
 
-	/// \address 0078BEA0
-    int TryParseChatCommands(const wchar_t* text, RECT &r, std::vector<void*>& v);
+    /// \address 0078BEA0
+    int TryParseChatCommands(const wchar_t *text, RECT &r, std::vector<void *> &v);
 
     /// \address 00787C10
     void sub_787C10(SChatMetaData &meta);
@@ -192,67 +194,66 @@ public:
 
 private:
 public:
-	CIRMManager m_IRM; //0x036C
-	char pad_0x039C[0xC]; //0x039C
-	CIFSupporterChatWnd* m_academyChatWindow; //0x03A8 
-	CIFTimerWnd* m_timerWindow; //0x03AC 
-	CIFQuickStateWnd* N00002636; //0x03B0 
-	CIFQuickStateHalfWnd* N00002637; //0x03B4 
-	char pad_0x03B8[0xC]; //0x03B8
+    CIRMManager m_IRM; //0x036C
+    char pad_0x039C[0xC]; //0x039C
+    CIFSupporterChatWnd *m_academyChatWindow; //0x03A8
+    CIFTimerWnd *m_timerWindow; //0x03AC
+    CIFQuickStateWnd *N00002636; //0x03B0
+    CIFQuickStateHalfWnd *N00002637; //0x03B4
+    char pad_0x03B8[0xC]; //0x03B8
 private:
-	int last_mouse_x; //0x03C4 3C4
-	int last_mouse_y; //0x03C8
-	char pad_03CC[1]; //0x03CC
-	char b_blockWASD; //0x03CD
-	char b_freeCameraMode; //0x03CE
-	char pad_03CF[45]; //0x03CF
-	class CCameraWorking* m_pCameraWorking; //0x03FC
-	class CIFCameraDataWnd* m_pCameraDataWnd; //0x0400
-	class CIFCameraDataMainWnd* m_pCameraDataMainWnd; //0x0404
-	char pad_0x0408[0xA4]; //0x0408
-	DWORD N00002675; //0x04AC 
-	DWORD N00002676; //0x04B0 
-	DWORD N00006601; //0x04B4 
-	char pad_0x04B8[0x4C]; //0x04B8
-	DWORD N0000268B; //0x0504 
-	char pad_0x0508[0x120]; //0x0508
-	int m_selectedObjectId; //0x0628 
-	char pad_0x062C[0x80]; //0x062C
+    int last_mouse_x; //0x03C4 3C4
+    int last_mouse_y; //0x03C8
+    char pad_03CC[1]; //0x03CC
+    char b_blockWASD; //0x03CD
+    char b_freeCameraMode; //0x03CE
+    char pad_03CF[45]; //0x03CF
+    class CCameraWorking *m_pCameraWorking; //0x03FC
+    class CIFCameraDataWnd *m_pCameraDataWnd; //0x0400
+    class CIFCameraDataMainWnd *m_pCameraDataMainWnd; //0x0404
+    char pad_0x0408[0xA4]; //0x0408
+    DWORD N00002675; //0x04AC
+    DWORD N00002676; //0x04B0
+    DWORD N00006601; //0x04B4
+    char pad_0x04B8[0x4C]; //0x04B8
+    DWORD N0000268B; //0x0504
+    char pad_0x0508[0x120]; //0x0508
+    int m_selectedObjectId; //0x0628
+    char pad_0x062C[0x80]; //0x062C
 public:
-	CNavigationDeadreckon m_Nav; //0x06AC
+    CNavigationDeadreckon m_Nav; //0x06AC
 private:
-	class CAutoPotion* m_pCAutoPotion; //0x06FC
-	char pad_0x0700[0x20]; //0x0700
-	CIFFade* m_screenCover; //0x0720 
-	CIF_NPCWindow* m_npcWindow; //0x0724 
-	CIFMainPopup* m_mainPopup; //0x0728 
-	CIFSkillPracticeBox* m_skillUpWindow; //0x072C 
-	CIFHelperBubbleWindow* m_helperWindow; //0x0730 
-	CIFHelperBubbleWindow* N00002717; //0x0734 
-	char pad_0x0738[0x10]; //0x0738
-	CMessageBoxManager m_messageBoxManager; //0x0748 
-	class CMessageTipManager* m_pCMessageTipManager; //0x0774 774
-	class CSkillCoolTimeManager* m_pCSkillCoolTimeManager; //0x0778
-	class CItemReuseDelayManager* m_pCItemReuseDelayManager; //0x077C
-	class CSkillRunTimeManager* m_pCSkillRunTimeManager; //0x0780
-	char pad_0784[0x48]; //0x0784
-	char pad_07cc[1]; // 0x7cc
+    class CAutoPotion *m_pCAutoPotion; //0x06FC
+    char pad_0x0700[0x20]; //0x0700
+    CIFFade *m_screenCover; //0x0720
+    CIF_NPCWindow *m_npcWindow; //0x0724
+    CIFMainPopup *m_mainPopup; //0x0728
+    CIFSkillPracticeBox *m_skillUpWindow; //0x072C
+    CIFHelperBubbleWindow *m_helperWindow; //0x0730
+    CIFHelperBubbleWindow *N00002717; //0x0734
+    char pad_0x0738[0x10]; //0x0738
+    CMessageBoxManager m_messageBoxManager; //0x0748
+    class CMessageTipManager *m_pCMessageTipManager; //0x0774 774
+    class CSkillCoolTimeManager *m_pCSkillCoolTimeManager; //0x0778
+    class CItemReuseDelayManager *m_pCItemReuseDelayManager; //0x077C
+    class CSkillRunTimeManager *m_pCSkillRunTimeManager; //0x0780
+    char pad_0784[0x48]; //0x0784
+    char pad_07cc[1]; // 0x7cc
 public:
-	undefined1 field_0x7cd;
+    undefined1 field_0x7cd;
 private:
-	char pad_07d0[0x24];
-	class CPreUseItem* m_pCPreUseItem; //0x07F4
-	char pad_07F8[92]; //0x07F8
+    char pad_07d0[0x24];
+
+    class CPreUseItem *m_pCPreUseItem; //0x07F4
+    char pad_07F8[92]; //0x07F8
 
 
-	BEGIN_FIXTURE()
+    BEGIN_FIXTURE()
         ENSURE_SIZE(0x854)
         ENSURE_OFFSET(m_pCSkillRunTimeManager, 0x780)
         ENSURE_OFFSET(field_0x7cd, 0x7cd)
         ENSURE_OFFSET(m_pCPreUseItem, 0x7f4)
     END_FIXTURE()
 
-	RUN_FIXTURE(CGInterface)
+    RUN_FIXTURE(CGInterface)
 };
-
-#define g_pCGInterface (*(CGInterface**)0x0110F80C)
