fa82ec597aa4a8211db04444efa16decbbe0d433
florian0 <florian0@protonmail.com>
2021-01-07 23:51:46 +0100

    Create StaticAddressHook

---
+++ source/third-party/remodel/include/remodel/StaticAddressHook.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#define HOOK_ORIGINAL_STATIC(address, newValue) \
+    static StaticAddressHook<address> hook_ ## address ((newValue));
+
+template<unsigned int Address>
+class StaticAddressHook {
+public:
+    explicit StaticAddressHook(const void *newValue) {
+        static int count = 0;
+
+        if (count != 0) {
+            throw "Too many registrations";
+        }
+
+        count++;
+
+        WriteToAddress(reinterpret_cast<void*>(Address), newValue);
+    }
+
+private:
+    template<typename T>
+    void WriteToAddress(void *address, T value) {
+        DWORD dwProtect = 0;
+
+        if (!VirtualProtect((LPVOID) address, sizeof(T), PAGE_EXECUTE_READWRITE, &dwProtect)) {
+            perror("Failed to unprotect memory\n");
+            return;
+        }
+
+        *reinterpret_cast<T*>(address) = value;
+
+        DWORD otherProtect;
+        if (!VirtualProtect((LPVOID) address, sizeof(T), dwProtect, &otherProtect)) {
+            perror("Failed to restore protection on memory");
+        }
+    }
+};
