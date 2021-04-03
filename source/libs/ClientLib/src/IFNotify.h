2f3522170f956948556a86952049f316d2f41bd8
florian0 <florian0@protonmail.com>
2021-04-04 01:23:11 +0200

    Create members of CIFNotify and setters
    These functions allow specifing the y position (distance from top) of the notification area
    and background color of text. Note that the color of borders is defined by individual
    textures set in CIFStretchWnd.

--- source/libs/ClientLib/src/IFNotify.h
+++ source/libs/ClientLib/src/IFNotify.h
@@ -9,12 +9,29 @@ public:
     /// \param msg Message to be shown
     void ShowMessage(const std::n_wstring &msg);
 
-private:
-    char _pad[0x18];
+    /// \brief Set the Y position of this element
+    /// \details This value is used for auto-positioning.
+    /// \address 007b3d40
+    void SetYPosition(int yposition);
+
+    /// \brief Sets the background color
+    /// \address 007b3d10
+    void SetColor(unsigned char red, unsigned char green, unsigned char blue);
 
 private:
-BEGIN_FIXTURE()
+    char pad7c8[16]; // 0x07c8
+    int m_yposition; // 0x07d4
+    unsigned char m_red; // 0x7d8
+    unsigned char m_green; // 0x7d9
+    unsigned char m_blue; // 0x7da
+
+private:
+    BEGIN_FIXTURE()
         ENSURE_SIZE(0x7dc)
+        ENSURE_OFFSET(m_yposition, 0x7d4)
+        ENSURE_OFFSET(m_red, 0x7d8)
+        ENSURE_OFFSET(m_green, 0x7d9)
+        ENSURE_OFFSET(m_blue, 0x7da)
     END_FIXTURE()
 
     RUN_FIXTURE(CIFNotify)
