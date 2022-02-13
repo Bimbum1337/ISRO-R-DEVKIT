129c64543454db6eeb64e5793e7d4c09a9c49a48
khalid sayed <khalid.201k@gmail.com>
2022-02-13 05:06:34 +0000

    Update SOItem.h

--- source/libs/ClientLib/src/SOItem.h
+++ source/libs/ClientLib/src/SOItem.h
@@ -28,13 +28,38 @@ public:
 public:
     char pad_0000[0x28];//0x0000
     undefined1 field_28;//0x0028
-    char pad_0029[0x34 - 0x29];//0x0025
+    char pad_0029[0xB];//0x0025
     int m_refObjItemId;//0x0034
-    char pad_0038[0x9c - 0x38];//0x0038
+    char pad_0038[0x54]; //0x0038
+	BYTE m_OptLevel; //0x008C
+	char pad_008D[0xB]; //0x008D
+	int m_CurrDurability; //0x0098
     int m_quantity; // 0x009c
-    char pad_00a0[0xc4 - 0xa0];//0x00a0
+    char pad_00A0[0x24]; //0x00A0
     std::n_map<Blue, int> bluemap;//0x00c4
-    char pad_00cc[464 - (0xc4 + 0xc)];
+    char pad_00D0[0x10]; //0x00D0
+	int m_PhyAtkPwrMax; //0x00E0
+	int m_PhyAtkPwrMin; //0x00E4
+	int m_MagAtkPwrMax; //0x00E8
+	int m_MagAtkPwrMin; //0x00EC
+	int m_BlockingRateValue; //0x00F0
+	int m_MaxDurability; //0x00F4
+	float m_PhyDefPwrValue; //0x00F8
+	int m_ParryRateValue; //0x00FC
+	float m_PhyAbsorption; //0x0100
+	char pad_0104[4]; //0x0104
+	int m_CriticalValue; //0x0108
+	float m_MagDefPwrValue; //0x010C
+	float m_MagAbsorption; //0x0110
+	float m_PhyReinforcementMin; //0x0114
+	float m_PhyReinforcementMax; //0x0118
+	float m_MagReinforcementMin; //0x011C
+	float m_MagReinforcementMax; //0x0120
+	float m_PhyReinforcementValue; //0x0124
+	float m_MagReinforcementValue; //0x0128
+	char pad_012C[4]; //0x012C
+	int m_AttackRateValue; //0x0130
+	char pad_0134[0x9C]; //0x0134
 
     BEGIN_FIXTURE()
         ENSURE_SIZE(464)// approved by CIFExchange::ctor
