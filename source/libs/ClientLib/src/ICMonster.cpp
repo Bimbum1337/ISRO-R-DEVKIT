4cebc450bb3304c18c099e7c32c0fa1eb3bfc040
b0ykoe <b0ykoe@ymail.com>
2022-04-08 22:01:33 +0200

    keeping the function uncommented in there since we've done that in all replaced functions

--- source/libs/ClientLib/src/ICMonster.cpp
+++ source/libs/ClientLib/src/ICMonster.cpp
@@ -1,7 +1,7 @@
 #include "ICMonster.h"
 
 void CICMonster::Func_15(int param_1, float* param_2) {
-    printf("%s\n", __FUNCTION__);
+    //printf("%s\n", __FUNCTION__);
     reinterpret_cast<void (__thiscall *)(const CICMonster *, int param_1, float *param_2)>(0x009d3e70)(this, param_1, param_2);
 }
 
